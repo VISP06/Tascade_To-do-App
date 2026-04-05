9516ddc9a5398f46727a785a6cff582680d87a89
Vishruth <notvishruth@gmail.com>
2026-04-05 22:11:37 +0530

    Minor improvements made to FAB

--- app/src/main/java/com/example/tascade/ui/list/TodoScreen.kt
+++ app/src/main/java/com/example/tascade/ui/list/TodoScreen.kt
@@ -8,16 +8,27 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,15 +37,16 @@ import androidx.compose.ui.unit.sp
 import com.example.tascade.R
 import com.example.tascade.model.Todo
 import com.example.tascade.ui.components.TodoCard
+import com.example.tascade.ui.theme.TascadeTheme
 import com.example.tascade.util.halftoneBackground
 import com.example.tascade.util.sampleTasks
 
 
-@Preview
 @Composable
 fun TodoApp(){
     Scaffold(
-        topBar = {TodoTopBar()}
+        topBar = {TodoTopBar()},
+        floatingActionButton = { TodoFAB() }
     ) { innerPadding->
         Box(
             modifier = Modifier
@@ -52,30 +64,69 @@ fun TodoApp(){
 }
 
 @Composable
+fun TodoList(tasks:List<Todo>, modifier: Modifier = Modifier, contentPaddingValues: PaddingValues){
+
+    LazyColumn(modifier = Modifier, contentPadding=contentPaddingValues) {
+        items(tasks){ sampleTask->
+            Spacer(Modifier.padding(8.dp))
+            TodoCard(sampleTask)
+        }
+    }
+}
+@Composable
 fun TodoTopBar(modifier:Modifier = Modifier){
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(140.dp)
-            .background(Color.Magenta)
+            .background(Color(0xFF7B1FA2))
             .border(width = 3.dp, color = Color.Black)
+            .padding(start = 36.dp, top = 40.dp)
     ){
+
         Text(
             text = stringResource(R.string.top_bar_title),
             color = Color.White,
-            fontWeight = FontWeight.Bold,
+            fontWeight = FontWeight.ExtraBold,
+            letterSpacing = 5.sp,
             fontSize = 64.sp,
-            modifier = modifier.rotate(-2f)
+            modifier = modifier
+
         )
     }
 }
 
 @Composable
-fun TodoList(tasks:List<Todo>, modifier: Modifier = Modifier, contentPaddingValues: PaddingValues){
-    LazyColumn(modifier = Modifier, contentPadding=contentPaddingValues) {
-        items(tasks){ sampleTask->
-            TodoCard(sampleTask)
-            Spacer(Modifier.padding(8.dp))
+fun TodoFAB(modifier:Modifier = Modifier) {
+    Box(
+        Modifier
+            .clip(CircleShape)
+            .background(Color.Black)
+            .offset(x = -4.dp, y = -4.dp)
+    ) {
+        LargeFloatingActionButton(
+            onClick = { },
+            shape = CircleShape,
+            containerColor = Color(0xFF1A237E),
+            modifier = Modifier.border(3.dp, Color.Black, shape = CircleShape)
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                "Floating action button.",
+                modifier = Modifier
+                    .size(50.dp),
+                tint = Color.White
+            )
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun TodoAppPreview() {
+    TascadeTheme {
+        TodoApp()
+    }
+}
+
+
