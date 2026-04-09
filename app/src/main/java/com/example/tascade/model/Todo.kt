66e2506cf0890a66333676868b8b55ab3751e5a8
Vishruth <notvishruth@gmail.com>
2026-04-09 18:19:11 +0530

    Finished working on Add Todo Dialog

--- app/src/main/java/com/example/tascade/model/Todo.kt
+++ app/src/main/java/com/example/tascade/model/Todo.kt
@@ -5,7 +5,6 @@ import androidx.annotation.StringRes
 data class Todo(
     val id:Int,
     val title:String,
-    val desc:String,
     val priority:Int,
     val isCompleted:Boolean = false
 )
--- app/src/main/java/com/example/tascade/ui/list/TodoScreen.kt
+++ app/src/main/java/com/example/tascade/ui/list/TodoScreen.kt
@@ -1,8 +1,10 @@
 package com.example.tascade.ui.list
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -18,11 +20,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.systemGestureExclusion
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -36,6 +41,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -53,12 +59,13 @@ import com.example.tascade.ui.theme.TascadeTheme
 import com.example.tascade.util.halftoneBackground
 
 val vm = TodoViewModel()
+
 @Composable
-fun TodoApp(){
+fun TodoApp() {
     Scaffold(
-        topBar = {TodoTopBar()},
+        topBar = { TodoTopBar() },
         floatingActionButton = { TodoFAB() }
-    ) { innerPadding->
+    ) { innerPadding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -68,15 +75,19 @@ fun TodoApp(){
 
         ) {
 
-            TodoList(vm.todos, contentPaddingValues =innerPadding)
+            TodoList(vm.todos, contentPaddingValues = innerPadding)
 
         }
     }
 }
 
 @Composable
-fun TodoList(tasks:List<Todo>, modifier: Modifier = Modifier, contentPaddingValues: PaddingValues){
-    if(tasks.isEmpty()){
+fun TodoList(
+    tasks: List<Todo>,
+    modifier: Modifier = Modifier,
+    contentPaddingValues: PaddingValues
+) {
+    if (tasks.isEmpty()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
@@ -90,9 +101,9 @@ fun TodoList(tasks:List<Todo>, modifier: Modifier = Modifier, contentPaddingValu
                 contentDescription = null
             )
         }
-    }else {
+    } else {
         LazyColumn(modifier = Modifier, contentPadding = contentPaddingValues) {
-            items(items = tasks, key = { task -> task.id }) { task ->
+            items(items = tasks) { task ->
                 Spacer(Modifier.padding(8.dp))
                 TodoCard(
                     task = task,
@@ -102,8 +113,9 @@ fun TodoList(tasks:List<Todo>, modifier: Modifier = Modifier, contentPaddingValu
         }
     }
 }
+
 @Composable
-fun TodoTopBar(modifier:Modifier = Modifier){
+fun TodoTopBar(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -111,7 +123,7 @@ fun TodoTopBar(modifier:Modifier = Modifier){
             .background(Color(0xFF7B1FA2))
             .border(width = 3.dp, color = Color.Black)
             .padding(start = 36.dp, top = 40.dp)
-    ){
+    ) {
 
         Text(
             text = stringResource(R.string.top_bar_title),
@@ -126,53 +138,91 @@ fun TodoTopBar(modifier:Modifier = Modifier){
 }
 
 @Composable
-fun AddTodoDialog(showDialog:Boolean, onDismiss: () -> Unit){
-    var userTodoInput by remember{mutableStateOf("")}
-    Dialog(onDismissRequest = {onDismiss()}) {
+fun AddTodoDialog(showDialog: Boolean, onDismiss: () -> Unit, modifier: Modifier = Modifier) {
+    var userTodoInput by remember { mutableStateOf("") }
+    Dialog(onDismissRequest = { onDismiss() }) {
         Box(
             modifier = Modifier
                 .clip(RoundedCornerShape(8.dp))
                 .background(Color.Black)
                 .padding(start = 5.dp, top = 5.dp)
-                .size(300.dp)
-        ){
-            Box(
+                .size(300.dp, 350.dp)
+
+        ) {
+            Column(
                 Modifier
                     .offset(x = -4.dp, y = -4.dp)
                     .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(8.dp))
                     .background(color = Color.White, shape = RoundedCornerShape(8.dp))
                     .fillMaxSize()
-                    .padding(vertical = 16.dp)
-            ){
+                    .padding(16.dp) //applies "breathing room" (Gap) to all 4 sides hence why fillMaxWidth in textfield was the right move
+            ) {
                 Text(
                     text = "New_Task",
+                    color = Color.Black,
                     fontWeight = FontWeight.Bold,
                     fontSize = 32.sp,
                     fontStyle = FontStyle.Italic,
                     modifier = Modifier.padding(start = 12.dp, top = 8.dp)
                 )
-                Column() {
-                    TextField(
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TextField(
                         value = userTodoInput,
                         onValueChange = { userTodoInput = it },
-                        label = {Text("Task Description")},
+                        label = { Text("Task Description") },
                         modifier = Modifier
                             .border(width = 3.dp, color = Color.Black)
                             .background(color = Color.DarkGray)
-                    )
-                    Row(){
+                            .weight(1f)
+                )
 
+                Spacer(Modifier.weight(0.1f))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    OutlinedButton(
+                        onClick = { onDismiss() },
+                        shape = RectangleShape,
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        border = BorderStroke(3.dp, color = Color.Black)
+                    ){
+                        Text(
+                            text = "Cancel",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    OutlinedButton(
+                        onClick = {
+                            vm.addTodo(userTodoInput)
+                                  onDismiss()
+                                  },
+                        shape = RectangleShape,
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1A237E)),
+                        border = BorderStroke(3.dp, color = Color.Black)
+                    ){
+                        Text(
+                            text = "Add Task",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
                     }
                 }
             }
         }
     }
 }
+
 @Composable
-fun TodoFAB(modifier:Modifier = Modifier) {
-    var showAddDialog by remember{ mutableStateOf<Boolean>(false) }
-    if(showAddDialog){
-        AddTodoDialog(showAddDialog, onDismiss = {showAddDialog = false})
+fun TodoFAB(modifier: Modifier = Modifier) {
+    var showAddDialog by remember { mutableStateOf<Boolean>(false) }
+    if (showAddDialog) {
+        AddTodoDialog(showAddDialog, onDismiss = { showAddDialog = false })
     }
     Box(
         Modifier
@@ -183,7 +233,7 @@ fun TodoFAB(modifier:Modifier = Modifier) {
         LargeFloatingActionButton(
             onClick = {
                 showAddDialog = true
-                      },
+            },
             shape = CircleShape,
             containerColor = Color(0xFF1A237E),
             modifier = Modifier.border(3.dp, Color.Black, shape = CircleShape)
@@ -198,7 +248,6 @@ fun TodoFAB(modifier:Modifier = Modifier) {
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
